81050863914b6c6281fe68c570ccb4a55a3c080a
c-peace <85606158+c-peace@users.noreply.github.com>
2023-04-19 11:27:33 +0000

    feat: Build New Image function(change text)

--- Sofo/tools.js
+++ Sofo/tools.js
@@ -1,5 +1,16 @@
 function uploadImage() {
 
+    const element = document.querySelector('#upload');
+
+    element.addEventListener('change', (event) => {
+        const target = event.target;
+        const files = target.files;
+        const file = files[0]
+
+        const test = document.getElementById('info_img');
+        test.textContent = file.name;
+    });
+
 }
 
 function sizeImage() {
@@ -17,3 +28,25 @@ function sizeFlag() {
 function addText() {
 
 }
+
+
+class Source {
+    constructor(type, name) {
+        this.type = type;
+        this.name = name;
+    }
+}
+
+
+// test code
+
+const element = document.querySelector('#upload');
+
+element.addEventListener('change', (event) => {
+    const target = event.target;
+    const files = target.files;
+    const file = files[0]
+
+    const test = document.getElementById('info_img');
+    test.textContent = file.name;
+});
