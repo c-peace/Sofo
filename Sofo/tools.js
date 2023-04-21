258a362e43a98b2759e53126c8f69b2107a640e8
c-peace <85606158+c-peace@users.noreply.github.com>
2023-04-21 02:11:46 +0000

    feat: canvas basic setting

--- Sofo/tools.js
+++ Sofo/tools.js
@@ -1,3 +1,22 @@
+function setCanvas() {
+
+    // basic setting
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 508;
+    canvas.height = 719;
+
+    // draw text area in canvas
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 1;
+    ctx.strokeRect(38, 43, 431, 66);
+    ctx.moveTo(38, 76);
+    ctx.lineTo(127, 76);
+    ctx.moveTo(127, 43);
+    ctx.lineTo(127, 109);
+    ctx.stroke();
+}
+
 function uploadImage() {
 
     // Image Box에 파일 이름을 업로드 하는 기능
@@ -46,4 +65,5 @@ class Source {
 
 
 // test code
-uploadImage()
+uploadImage();
+setCanvas();
