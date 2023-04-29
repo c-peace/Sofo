91e0742ebf7cddb9b41fca66a57fd648a698734b
c-peace <85606158+c-peace@users.noreply.github.com>
2023-04-29 08:03:25 +0000

    feat: build auto set image size, draw text funtion

--- Sofo/tools.js
+++ Sofo/tools.js
@@ -1,47 +1,3 @@
-function setCanvas() {
-}
-
-
-
-function uploadImage() {
-
-    // Image Box에 파일 이름을 업로드 하는 기능
-    const element = document.querySelector('#upload');
-
-    element.addEventListener('change', (event) => {
-        const target = event.target;
-        const files = target.files;
-        const file = files[0]
-
-        const test = document.getElementById('info_img');
-        test.textContent = file.name;
-        // file input disable시키기
-        element.disabled = true;
-    });
-}
-
-// image 삭제시 이미지 삭제
-
-// image 삭제시 disable -> able로 변경
-
-
-function sizeImage() {
-
-}
-
-function addFlag() {
-
-}
-
-function sizeFlag() {
-
-}
-
-function addText() {
-
-}
-
-
 class Source {
     constructor(type, name) {
         this.type = type;
@@ -49,8 +5,6 @@ class Source {
     }
 }
 
-
-// test code
 
 // basic setting
 const canvas = document.querySelector('canvas');
@@ -63,24 +17,21 @@ canvas.height = 1684;
 // draw text area in canvas
 ctx.fillStyle = "white"; // 색상명
 ctx.fillRect(0, 0, 1190, 1684);
-ctx.strokeStyle = "#000000";
-ctx.lineWidth = 3;
-ctx.strokeRect(94, 80, 1002, 140);
-ctx.moveTo(94, 155);
-ctx.lineTo(300, 155);
-ctx.moveTo(300, 80);
-ctx.lineTo(300, 220);
-ctx.stroke();
+function drawTextArea() {
+    ctx.strokeStyle = "#000000";
+    ctx.lineWidth = 3;
+    ctx.fillRect(94, 80, 1002, 140);
+    ctx.strokeRect(94, 80, 1002, 140);
+    ctx.moveTo(94, 155);
+    ctx.lineTo(300, 155);
+    ctx.moveTo(300, 80);
+    ctx.lineTo(300, 220);
+    ctx.stroke();
+}
+drawTextArea();
 
-const imageRange = document.querySelector('#range_image');
-let imageRangeValue = 1;
 
-imageRange.addEventListener('change', (event) => {
-    imageRangeValue = event.target.value;
-    console.log(imageRangeValue);
-
-});
-
+// load Music Score
 function loadImage(event) {
     const file = event.target.files[0];
     const url = URL.createObjectURL(file);
@@ -88,13 +39,22 @@ function loadImage(event) {
     console.dir(file);
     image.src = url;
     image.onload = function () {
-        const imageHeight = image.height;
-        const imageWidth = image.width;
+
+        // set image size
+        let imageHeight = 0;
+        let imageWidth = 0;
+        if (image.height > image.width) {
+            imageHeight = (canvas.height - 220) * 9 / 10;
+            imageWidth = image.width * imageHeight / image.height;
+        } else {
+            imageWidth = canvas.width * 9 / 10;
+            imageHeight = image.height * imageWidth / image.width;
+        }
         ctx.drawImage(image, (canvas.width - imageWidth) / 2, (220 + canvas.height - imageHeight) / 2, imageWidth, imageHeight);
     };
 
-    const test = document.getElementById('info_img');
-    test.textContent = file.name;
+    const infoImage = document.getElementById('info_img');
+    infoImage.textContent = file.name;
     // file input disable시키기
     imageInput.disabled = true;
 
@@ -102,6 +62,47 @@ function loadImage(event) {
 
 imageInput.addEventListener('change', loadImage);
 
+// keyboard
+let i = 0;
+const infoText = document.getElementById('info_text');
+const keyboard = document.querySelector('#keyboard');
+function onTextClick() {
+    infoText.textContent = '입력중 . . .';
+    drawTextArea();
+    keyboard.disabled = false;
+    keyboard.placeholder = 'Typing... Enter!';
+    i = 0;
+}
+
+const dictText = {}
+function enterkey() {
+    if (window.event.keyCode == 13) {
+        // 엔터키가 눌렸을 때
+        if (i < 3) {
+            dictText[i] = keyboard.value;
+            i++;
+        }
+
+        if (i == 3) {
+            infoText.textContent = '입력완료!';
+            ctx.save();
+            ctx.lineWidth = 1;
+            ctx.fillStyle = 'black';
+            ctx.font = '42px Arial';
+            ctx.fillText(dictText[0], 110, 135);
+            ctx.font = '30px Arial';
+            ctx.fillText('tempo = ' + dictText[1], 110, 200);
+            ctx.font = '40px Arial';
+            ctx.fillText(dictText[2], 350, 165);
+            ctx.restore();
+            keyboard.disabled = true;
+            keyboard.placeholder = 'Sofo - 찬양팀 세션을 위한 악보편집 서비스';
+        }
+        keyboard.value = ''
+    }
+}
+
+// modal page
 function fullScreen() {
     const modal = document.querySelector('#modal');
 
@@ -119,6 +120,8 @@ function backScreen() {
     modal.style.display = 'none';
 }
 
+
+// download
 function onSaveClick() {
     const url = canvas.toDataURL();
     console.dir(url);
@@ -126,6 +129,6 @@ function onSaveClick() {
     Fullscreen.src = url;
     const a = document.createElement("a");
     a.href = url;
-    a.download = "myDrawing.png";
+    a.download = "musicScore.png";
     a.click();
 }
