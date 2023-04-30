5ffd725d059d2428c67fac7daeca5bdaef42d038
c-peace <85606158+c-peace@users.noreply.github.com>
2023-04-30 11:28:38 +0000

    feat: build canvas layer function

--- Sofo/tools.js
+++ Sofo/tools.js
@@ -1,6 +1,5 @@
-
 // basic setting
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector('#canvasMain');
 const ctx = canvas.getContext('2d');
 const imageInput = document.getElementById('upload');
 canvas.width = 1190;
@@ -11,6 +10,7 @@ canvas.height = 1684;
 ctx.fillStyle = "white"; // 색상명
 ctx.fillRect(0, 0, 1190, 1684);
 function drawTextArea() {
+    ctx.beginPath();
     ctx.strokeStyle = "#000000";
     ctx.lineWidth = 3;
     ctx.fillRect(94, 80, 1002, 140);
@@ -55,10 +55,154 @@ function loadImage(event) {
 
 imageInput.addEventListener('change', loadImage);
 
-// keyboard
+
+// Draw Flag
+const canvasFlag = document.getElementById("canvasFlag");
+const ctxFlag = canvasFlag.getContext('2d');
+canvasFlag.width = 1190;
+canvasFlag.height = 1684;
+canvas.backScreen = 'transparent';
+
+let dragok = false;
+let startX;
+let startY;
+
+canvasFlag.addEventListener('mousedown', myDown);
+canvasFlag.addEventListener('mouseup', myUp);
+canvasFlag.addEventListener('mousemove', myMove);
+
+const shapes = [];
+shapes.push({ x: 100, y: 1600, r: 30, strokeStyle: "black", fillStyle: "white", name: "A", isDragging: false });
+
+const infoFlag = document.getElementById('info_flag');
+const keyboard = document.querySelector('#keyboard');
+
+function onFlagClick() {
+    infoFlag.textContent = '생성중 . . .';
+    keyboard.disabled = false;
+    keyboard.focus();
+    keyboard.placeholder = 'Typing... Enter!';
+    i = 4;
+}
+
+function createFlag(nameFlag) {
+    shapes.push({ x: 25 * i, y: 1600, r: 30, strokeStyle: "black", fillStyle: "white", name: nameFlag, isDragging: false });
+}
+
+function circle(c) {
+    ctxFlag.save();
+    ctxFlag.beginPath();
+    // outlind
+    ctxFlag.lineWidth = 8;
+    ctxFlag.strokeStyle = c.strokeStyle;
+    ctxFlag.arc(c.x, c.y, c.r, 0, 2 * Math.PI);
+    ctxFlag.stroke();
+    // inner fill
+    ctxFlag.fillStyle = c.fillStyle;
+    ctxFlag.fill();
+    // text
+    ctxFlag.textAlign = 'center';
+    ctxFlag.lineWidth = 1;
+    ctxFlag.fillStyle = c.strokeStyle;
+    ctxFlag.font = '40px Arial';
+    ctxFlag.fillText(c.name, c.x, c.y + c.r * 1 / 3);
+    ctxFlag.restore();
+}
+
+function clear() {
+    ctxFlag.clearRect(0, 0, canvasFlag.width, canvasFlag.height);
+}
+
+function draw() {
+    clear();
+    for (let i = 0; i < shapes.length; i++) {
+        circle(shapes[i]);
+    }
+}
+
+// handle mousedown events
+function myDown(e) {
+    // tell the browser we're handling this mouse event
+    e.preventDefault();
+    e.stopPropagation();
+
+    // get the current mouse position
+    const mx = (e.offsetX / ((window.innerHeight * 595 * 9) / (969 * 10))) * canvasFlag.width;
+    const my = (e.offsetY / ((window.innerHeight * 842 * 9) / (969 * 10))) * canvasFlag.height;
+
+    // test each shape to see if mouse is inside
+    dragok = false;
+    for (let i = 0; i < shapes.length; i++) {
+        let s = shapes[i];
+        const dx = s.x - mx;
+        const dy = s.y - my;
+
+        // test if the mouse is inside this circle
+        if (!dragok && dx * dx + dy * dy < s.r * s.r) {
+            dragok = true;
+            s.isDragging = true;
+
+        }
+    }
+    // save the current mouse position
+    startX = mx;
+    startY = my;
+}
+
+// handle mouseup events
+function myUp(e) {
+    // tell the browser we're handling this mouse event
+    e.preventDefault();
+    e.stopPropagation();
+
+    // clear all the dragging flags
+    dragok = false;
+    for (let i = 0; i < shapes.length; i++) {
+        shapes[i].isDragging = false;
+    }
+}
+
+// handle mouse moves
+function myMove(e) {
+    // if we're dragging anything...
+    if (dragok) {
+        // tell the browser we're handling this mouse event
+        e.preventDefault();
+        e.stopPropagation();
+
+        // get the current mouse position
+        const mx = (e.offsetX / ((window.innerHeight * 595 * 9) / (969 * 10))) * canvasFlag.width;
+        const my = (e.offsetY / ((window.innerHeight * 842 * 9) / (969 * 10))) * canvasFlag.height;
+
+        // calculate the distance the mouse has moved
+        // since the last mousemove
+        const dx = mx - startX;
+        const dy = my - startY;
+
+        // move each rect that isDragging
+        // by the distance the mouse has moved
+        // since the last mousemove
+        for (let i = 0; i < shapes.length; i++) {
+            const s = shapes[i];
+            if (s.isDragging) {
+                s.x += dx;
+                s.y += dy;
+            }
+        }
+
+        // redraw the scene with the new rect positions
+        draw();
+
+        // reset the starting mouse position for the next mousemove
+        startX = mx;
+        startY = my;
+    }
+}
+
+
+// Draw Text
 let i = 0;
 const infoText = document.getElementById('info_text');
-const keyboard = document.querySelector('#keyboard');
 function onTextClick() {
     infoText.textContent = '입력중 . . .';
     drawTextArea();
@@ -66,6 +210,19 @@ function onTextClick() {
     keyboard.focus();
     keyboard.placeholder = 'Typing... Enter!';
     i = 0;
+}
+
+function drawText() {
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'black';
+    ctx.font = '42px Arial';
+    ctx.fillText(dictText[0], 110, 135);
+    ctx.font = '30px Arial';
+    ctx.fillText('tempo = ' + dictText[1], 110, 200);
+    ctx.font = '40px Arial';
+    ctx.fillText(dictText[2], 350, 165);
+    ctx.restore();
 }
 
 const dictText = {}
@@ -79,18 +236,17 @@ function enterkey() {
 
         if (i == 3) {
             infoText.textContent = '입력완료!';
-            ctx.save();
-            ctx.lineWidth = 1;
-            ctx.fillStyle = 'black';
-            ctx.font = '42px Arial';
-            ctx.fillText(dictText[0], 110, 135);
-            ctx.font = '30px Arial';
-            ctx.fillText('tempo = ' + dictText[1], 110, 200);
-            ctx.font = '40px Arial';
-            ctx.fillText(dictText[2], 350, 165);
-            ctx.restore();
+            drawText();
             keyboard.disabled = true;
             keyboard.placeholder = 'Sofo - 찬양팀 세션을 위한 악보편집 서비스';
+        }
+
+        if (i > 3) {
+            if (keyboard.value != '') {
+                createFlag(keyboard.value, i);
+                draw();
+                i++;
+            }
         }
         keyboard.value = ''
     }
